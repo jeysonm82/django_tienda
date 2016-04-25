98c37d73e29a083501b7d9e185b1d76355902a7f
jeysonmc <jeyson.molina.co@gmail.com>
2016-04-25 10:24:32 -0500

    Discount admin

--- tienda/admin/catalog.py
+++ tienda/admin/catalog.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
-from tienda.models import CategoryImage, Category, CatalogDiscount #, CatalogDiscountRule
+from tienda.models import CategoryImage, Category, CatalogDiscount, CatalogDiscountRule
 from django import forms
 
 class CategoryImageInline(admin.TabularInline):
@@ -21,12 +21,11 @@ class CategoryAdmin(MPTTModelAdmin):
     tabbed_name.allow_tags = True
     tabbed_name.short_description = Category._meta.verbose_name
 
-'''
 class CatalogDiscountRuleInline(admin.StackedInline):
     model = CatalogDiscountRule
-    extra = 1
-    fields = ['use_or', 'rtype', 'category', 'product']
-'''
+    extra = 0
+    fields = ['rtype', 'category', 'product']
+    filter_horizontal = ('category','product')
 
 class DaysSelectorWidget(forms.CheckboxSelectMultiple):
     DAYS = ((1, 'Lunes'),(2, 'Martes'),(3, 'Miércoles'), (4, 'Jueves'),
@@ -54,8 +53,14 @@ class CatalogDiscountForm(forms.ModelForm):
 
 class CatalogDiscountAdmin(admin.ModelAdmin):
     model = CatalogDiscount
-    #inlines = [CatalogDiscountRuleInline]
-    #change_form_template = 'more/admin/change_form.html'
-    list_display = ('id', 'name', 'description','date_from', 'date_to', 'enabled')
+    inlines = [CatalogDiscountRuleInline]
+    change_form_template = 'tienda/change_form.html'
+    list_display = ('id', 'name', 'description', 'discount', 'discount_unit', 'date_from', 'date_to', 'days_formatted', 'enabled')
     list_display_links = ('id', 'name')
     form = CatalogDiscountForm
+
+
+    def days_formatted(self, obj):
+        return ', '.join([DaysSelectorWidget.DAYS[int(x)-1][1] for x in obj.days.split(',')])
+
+    days_formatted.short_description = u"Días"
